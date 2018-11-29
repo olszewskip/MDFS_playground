74001312e274335f5c0d3a5bf68a4c9f15f7942f
olszewskip <olspaw@gmail.com>
2018-11-29 21:47:08 +0100

    First commit

---
+++ R/write_mad_sample_IG.R
@@ -0,0 +1,7 @@
+mad <- read.csv('mad_sample.csv')
+mad_X <- mad[,1:3]
+mad_Y <- mad[,4]
+divs <- 3
+out <- MDFS::ComputeMaxInfoGains(mad_X,mad_Y, dimensions=2, divisions = divs, range=1, return.tuples = T)
+write.csv(out, file='mad_sample_IG.csv', row.names=FALSE)
+out
