7a90bd36bd0595ff56d9e3f1b92e070b3d3d4113
olszewskip <olspaw@gmail.com>
2018-11-30 18:48:31 +0100

    Playing with ColumnBunches23 in python

--- R/write_mad_sample_IG.R
+++ R/write_mad_sample_IG.R
@@ -1,6 +1,6 @@
 mad <- read.csv('mad_sample.csv')
-mad_X <- mad[,1:3]
-mad_Y <- mad[,4]
+mad_X <- mad[,1:10]
+mad_Y <- mad[,11]
 divs <- 3
 out <- MDFS::ComputeMaxInfoGains(mad_X,mad_Y, dimensions=2, divisions = divs, range=1, return.tuples = T)
 write.csv(out, file='mad_sample_IG.csv', row.names=FALSE)
