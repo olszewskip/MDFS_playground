e7b5414ff45c3d3fe6040023a410e7fdb196eeff
olszewskip <olspaw@gmail.com>
2018-12-18 00:38:02 +0100

    make consistent

--- C/create_del_mat3D.c
+++ C/create_del_mat3D.c
@@ -27,6 +27,7 @@ void del3D(int***M, size_t dim0, size_t dim1, size_t dim2) {
     free(M);
 }
 
+// why doesn't it work?
 void spawn3D(int***M, size_t dim0, size_t dim1, size_t dim2) {
 
     M = malloc(dim0 * sizeof(*M));
@@ -39,23 +40,23 @@ void spawn3D(int***M, size_t dim0, size_t dim1, size_t dim2) {
     }
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
+
     size_t dim0, dim1, dim2;
     dim0 = 2;
     dim1 = 3;
     dim2 = 1;
 
-	int ***A = matrix3D(dim0, dim1, dim2); 
+    int ***A = matrix3D(dim0, dim1, dim2); 
 
-	A[1][2][0] = 123;
-	printf("%d\n", A[1][2][0]);
+    A[1][2][0] = 123;
+    printf("%d\n", A[1][2][0]);
 
     del3D(A, dim0, dim1, dim2);
 
     int ***B = NULL;
     spawn3D(B, dim0, dim1, dim2);
-    B[1][2][0] = 123;
-    
-	return 0;
+    B[1][2][0] = 123; // error
+
+    return 0;
 }
--- C/create_del_mat3D.cpp
+++ C/create_del_mat3D.cpp
@@ -35,7 +35,7 @@ void spawn3D(int*** M, int dim0, int dim1, int dim2) {
     
     for(int i=0; i<dim0; i++) {
         M[i] = new int*[dim1];
-            for(int j=0; j<dim1; j++) {
+            for (int j=0; j<dim1; j++) {
                 M[i][j] = new int[dim2];
             }
     }
@@ -44,35 +44,21 @@ void spawn3D(int*** M, int dim0, int dim1, int dim2) {
 
 int main(int argc, char **argv) {
 
-    cout << "1)\n";
-    int*** A = NULL;
+    size_t dim0, dim1, dim2;
+    dim0 = 2;
+    dim1 = 3;
+    dim2 = 1;
 
-    cout << "2)\n";
-    A = matrix3D(2,3,1);
+    int ***A = matrix3D(dim0, dim1, dim2);
 
-    cout << "3)\n";
     A[1][2][0] = 123;
-
-    cout << "4)\n";
     printf("%d\n", A[1][2][0]);
 
-    cout << "5)\n";
     del3D(A, 2,3,1);
 
-    //cout << "6)\n";
-    //printf("%d\n", A[1][2][0]); //error
-
-    cout << "7)\n";
     int*** B = NULL;
-
-    cout << "8)\n";
     spawn3D(B, 2,3,1);
-
-    cout << "9)\n";
-    B[1][2][0] = 123; //why error?
-
-    cout << "10)\n";
-    printf("%d\n", B[1][2][0]);
+    B[1][2][0] = 123; // error
 
     return 0;
 } 
