ef8a184c0dd02974944257ad78458f6ef748eee2
olszewskip <olspaw@gmail.com>
2018-12-21 21:15:17 +0100

    First ttys at rading in the csv

--- R/write_mad_sample.R
+++ R/write_mad_sample.R
@@ -4,3 +4,5 @@ mad_sample_X <- mad$data[1:n,1:10]
 mad_sample_Y <- mad$decision[1:n]
 mad_sample_XY <- data.frame(X = mad_sample_X, Y = mad_sample_Y)
 write.csv(mad_sample_XY, file='mad_sample.csv', row.names=FALSE)
+
+write.table(mad, file = "madelon.csv", row.names=FALSE, col.names=FALSE)
