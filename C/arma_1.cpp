c5d3d6246f9d992fbd1f282753727b842b01d510
olszewskip <olspaw@gmail.com>
2019-01-02 20:06:43 +0100

    After ubuntu reinstall

---
+++ C/arma_1.cpp
@@ -0,0 +1,23 @@
+#include <armadillo>
+#include <iostream>
+
+int main(int argc, const char **argv) {
+  arma::Mat<double> M;
+  M.load("madelon.csv");
+
+  std::cout << "first element: " << M(0, 0) << std::endl;
+  std::cout << "number of rows: " << M.n_rows << std::endl;
+  std::cout << "number of cols: " << M.n_cols << std::endl;
+  std::cout << "number of elems: " << M.n_elem << std::endl;
+  std::cout << "dimensions together: " << arma::size(M) << std::endl;
+
+  std::cout
+      << "number of elements in the 1st columns that are greater than 480: "
+      << arma::sum(M.col(0) > 480) << std::endl;
+
+  arma::Mat<double> M2 = M.cols(1, 2);
+  M2(0, 0) = 123;
+  std::cout << "first element again: " << M(0, 0) << std::endl;
+
+  return 0;
+}
---
+++ C/readin_1.cpp
@@ -0,0 +1,30 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+  ifstream in("simple.csv");
+  vector<vector<double>> fields;
+
+  if (in) {
+    string line;
+
+    while (getline(in, line)) {
+      stringstream sep(line);
+      string field;
+      fields.push_back(vector<double>());
+      while (getline(sep, field, ',')) {
+        fields.back().push_back(stod(field));
+      }
+    }
+  }
+  for (auto row : fields) {
+    for (auto field : row) {
+      cout << field << ' ';
+    }
+    cout << '\n';
+  }
+}
