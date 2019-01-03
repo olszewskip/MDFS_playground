11f1478dbf03da5dc0b489d5681d5778b088df23
olszewskip <olspaw@gmail.com>
2019-01-03 22:20:10 +0100

    fooling around with reading in

--- C/readin_1.cpp
+++ C/readin_1.cpp
@@ -7,6 +7,15 @@
 #include <string>
 #include <vector>
 
+void printM(std::vector<std::vector<double>> *M) {
+  for (auto row : *M) {
+    for (auto num : row) {
+      std::cout << num << " ";
+    }
+    std::cout << '\n';
+  }
+}
+
 int main() {
   // using namespace std;
 
@@ -17,24 +26,71 @@ int main() {
   if (in) {
     std::string line;
 
-    while (getline(in, line)) {
+    while (std::getline(in, line)) {
       std::stringstream nums(line);
       std::string num;
       M.emplace_back(std::vector<double>());
-      while (getline(nums, num, ',')) {
+      while (std::getline(nums, num, ',')) {
+        // std::cout << stod(num) << '\n';
         M.back().emplace_back(stod(num));
       }
     }
   }
-  in.close();
+  in.close();  // should I do this?
+
+  ///
+  std::cout << M.size() << " " << M[0].size() << std::endl;
+  printM(&M);
+
+  // open file, create vector of vectors Mt
+  std::ifstream in2("simple.csv");
+  std::vector<std::vector<double>> Mt;
+  // stream from file to Mt in a transposed order
+
+  // prepare global variables (I guess... ?)
+  std::string line;
+  std::string num;
+
+  // fill the first column of Mt with first line from the csv
+  std::getline(in2, line);
+  std::stringstream firstnums(line);
+  while (std::getline(firstnums, num, ',')) {
+    std::vector<double> row = {stod(num)};
+    Mt.emplace_back(row);
+  }
+
+  // printM(&Mt);
+
+  // fill other columns with proceeding lines from the csv
+  while (std::getline(in2, line)) {
+    std::stringstream nums(line);
+    int i = 0;
+    for (auto &row : Mt) {
+      /* get num */ std::getline(nums, num, ',');
+      std::cout << i << " " << stod(num) << std::endl;
+      i++;
+      // row.emplace_back(0.0);
+    }
+  }
+
+  printM(&Mt);
+
+  // if (in) {
+  //   std::string line;
+
+  //   while (getline(in, line)) {
+  //     std::stringstream nums(line);
+  //     std::string num;
+  //     M.emplace_back(std::vector<double>());
+  //     while (getline(nums, num, ',')) {
+  //       M.back().emplace_back(stod(num));
+  //     }
+  //   }
+  // }
+  //
 
   // print M to screen
-  for (auto row : M) {
-    for (auto num : row) {
-      std::cout << num << " ";
-    }
-    std::cout << '\n';
-  }
+  printM(&M);
 
   std::cout << M.size() << std::endl;  // 5 ??
   std::cout << M[0].size() << std::endl;
@@ -53,6 +109,4 @@ int main() {
   M[0][0] = 123.;
   std::cout << "M[0][0]=" << M[0][0] << " ,  M2[0][0]=" << M2[0][0]
             << std::endl;
-
-  
 }
