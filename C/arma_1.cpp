282d9120fdd2ed7585bac50376345704db2ddc72
olszewskip <olspaw@gmail.com>
2019-01-03 12:54:53 +0100

    fun with coping cols in arma

--- C/arma_1.cpp
+++ C/arma_1.cpp
@@ -19,9 +19,14 @@ int main(int argc, const char **argv) {
       << "number of elements in the 1st columns that are greater than 480: "
       << arma::sum(M.col(0) > 480) << std::endl;
 
-  arma::Mat<double> M2 = M.cols(1, 2);
+  arma::Mat<double> M2 = M.cols(0, 2);
+  std::cout << "number of columns in the new copy: " << M2.n_cols << std::endl;
   M2(0, 0) = 123;
-  std::cout << "first element again: " << M(0, 0) << std::endl;
+  std::cout << "first element of the copy: " << M2(0, 0) << std::endl;
+  std::cout << "first element of the original: " << M(0, 0) << std::endl;
+
+  // overflowing copy: runtime error
+  arma::Mat<double> M3 = M.cols(M.n_cols, M.n_cols + 1);
 
   return 0;
 }
