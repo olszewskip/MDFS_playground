6c80d34a1ceca310c5b94a885ac3b033413a577f
olszewskip <olspaw@gmail.com>
2019-01-04 13:41:07 +0100

    print out two elems

--- C/create_del_mat3D.cpp
+++ C/create_del_mat3D.cpp
@@ -1,64 +1,62 @@
+// Copyright: me
 #include <iostream>
 using namespace std;
 
 int*** matrix3D(size_t dim0, size_t dim1, size_t dim2) {
-// use size_t or int?
+  // use size_t or int?
 
-    int*** M = NULL;
-    M = new int**[dim0];
+  int*** M = NULL;
+  M = new int**[dim0];
 
-    for(int i=0; i<dim0; i++) {
-        M[i] = new int*[dim1];
-            for(int j=0; j<dim1; j++) {
-                M[i][j] = new int[dim2];
-            }
+  for (int i = 0; i < dim0; i++) {
+    M[i] = new int*[dim1];
+    for (int j = 0; j < dim1; j++) {
+      M[i][j] = new int[dim2];
     }
+  }
 
-    return M;
+  return M;
 }
 
-void del3D(int***M, size_t dim0, size_t dim1, size_t dim2) {
-
-    for (int i=0; i<dim0; i++) {
-        for (int j=0; j<dim1; j++) {
-            delete[] M[i][j];
-        }
-        delete[] M[i];
+void del3D(int*** M, size_t dim0, size_t dim1, size_t dim2) {
+  for (int i = 0; i < dim0; i++) {
+    for (int j = 0; j < dim1; j++) {
+      delete[] M[i][j];
     }
-    delete[] M;
+    delete[] M[i];
+  }
+  delete[] M;
 }
 
 // why doesn't it work?
-void spawn3D(int*** *M, int dim0, int dim1, int dim2) {
+void spawn3D(int**** M, int dim0, int dim1, int dim2) {
+  *M = new int**[dim0];
 
-    *M = new int**[dim0];
-    
-    for(int i=0; i<dim0; i++) {
-        (*M)[i] = new int*[dim1];
-            for (int j=0; j<dim1; j++) {
-                (*M)[i][j] = new int[dim2];
-            }
+  for (int i = 0; i < dim0; i++) {
+    (*M)[i] = new int*[dim1];
+    for (int j = 0; j < dim1; j++) {
+      (*M)[i][j] = new int[dim2];
     }
+  }
 }
 
+int main(int argc, char** argv) {
+  size_t dim0, dim1, dim2;
+  dim0 = 2;
+  dim1 = 3;
+  dim2 = 1;
 
-int main(int argc, char **argv) {
+  int*** A = matrix3D(dim0, dim1, dim2);
 
-    size_t dim0, dim1, dim2;
-    dim0 = 2;
-    dim1 = 3;
-    dim2 = 1;
+  A[1][2][0] = 123;
+  printf("%d\n", A[1][2][0]);
 
-    int ***A = matrix3D(dim0, dim1, dim2);
+  del3D(A, 2, 3, 1);
 
-    A[1][2][0] = 123;
-    printf("%d\n", A[1][2][0]);
+  int*** B = NULL;
+  spawn3D(&B, 2, 3, 1);
+  B[1][2][0] = 123;
+  printf("%d\n", B[1][2][0]);
 
-    del3D(A, 2,3,1);
-
-    int*** B = NULL;
-    spawn3D(&B, 2,3,1);
-    B[1][2][0] = 123; // error
-
-    return 0;
-} 
+  return 0;
+}
