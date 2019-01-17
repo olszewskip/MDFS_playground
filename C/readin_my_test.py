10d5e2bb81749631220c503a38483c02f5adccb6
olszewskip <olspaw@gmail.com>
2019-01-17 14:21:37 +0100

    timing with python

--- C/readin_my_test.py
+++ C/readin_my_test.py
@@ -1,13 +1,28 @@
+# import time
+# import numpy
+
+# runs = 100
+# cols = [9, 99, 199]
+
+# tick = time.time()
+# for _ in range(runs):
+#     M = numpy.loadtxt("madelon.csv", delimiter=',')
+#     column_bunch = M[:, cols].copy()
+# tock = time.time()
+
+# print(f"{runs} runs finished in: {tock-tick}s")
+
 import time
 import numpy
+import pandas
 
-runs = 5
-cols = [200, 500]
+runs = 100
+cols = [9, 99, 199]
 
 tick = time.time()
 for _ in range(runs):
-    M = numpy.loadtxt("madelon.csv", delimiter=',')
-    column_bunch = M[:, cols].copy()
+    M = pandas.read_csv("madelonX4.csv")
+    column_bunch = M.iloc[:, cols].copy()
 tock = time.time()
 
-print(f"Average time of {runs} runs: {round((tock-tick)/runs, 3)}s")
+print(f"{runs} runs finished in: {tock-tick}s")
