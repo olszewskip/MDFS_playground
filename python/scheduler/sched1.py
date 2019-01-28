e696c36309559ea2e9defc67e4cead091a2148ed
olszewskip <olspaw@gmail.com>
2019-01-28 20:27:44 +0100

    toy-scheduler kinda working

--- python/scheduler/sched1.py
+++ python/scheduler/sched1.py
@@ -2,7 +2,6 @@ import numpy as np
 from scipy.stats import rankdata
 from pandas import read_csv
 from itertools import chain, starmap
-# np.random.seed(123)
 
 from mpi4py import MPI
 comm = MPI.COMM_WORLD
@@ -11,18 +10,11 @@ time0 = MPI.Wtime()
 size = comm.Get_size()
 rank = comm.Get_rank()
 
-#tag_idle = 100
-tag_results = 101
-tag_tile = 102
-tag_terminate = 103
-
-
+k = 3
+window = 10
 divisions = 10
 range_ = 0.1
 seed = 123
-k = 3
-#window = 10
-# M = (N-1) // window + 1
 
 print(rank, "starting")
 
@@ -52,7 +44,9 @@ else:
     data_shape = None    
 
 dim0, dim1 = comm.bcast(data_shape, root=0)
-print(rank, "dims:", dim0, dim1)
+M = (dim0 - 1) // window + 1
+
+print(rank, "dims:", dim0, dim1, "tile_index_span:", M)
 
 if rank != 0:
     data = np.empty((dim0, dim1), dtype='int64')
@@ -60,6 +54,7 @@ if rank != 0:
 comm.Bcast([data, MPI.INT], root=0)
 if rank == 0:
     print("Elapsed:", MPI.Wtime() - time0, "sec")
+    
     
 def tiles_generator(k, M, skip_diag=True):
     '''
@@ -102,24 +97,18 @@ if rank == 0:
         for index, score in results.items():
             if score[0] > final_results[index][0]:
                 final_results[index] = score
-        
-    #for _ in range(size - 1):
-    #    next(local_tiles)
-    #dummy_queue = [next(local_tiles), next(local_tiles), next(local_tiles), next(local_tiles), next(local_tiles)]
     
-    print(rank, "entering for loop")
+    print(rank, "entering the for loop")
     status = MPI.Status()
     for tile in tiles_generator(k, dim0):
-        results = comm.recv(status=status) # source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, 
+        results = comm.recv(status=status)
         comm.isend(tile, dest=status.source)
-        #print(rank, "received", status.tag, "from", status.source)
         record(results)
     
     print("Elapsed:", MPI.Wtime() - time0, "sec")
     print(rank, "starts terminating workers")
     for _ in range(size - 1):
-        last_results = comm.recv(status=status) # source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG
-        #print(rank, "received", status.tag, "from", status.source)
+        last_results = comm.recv(status=status)
         record(last_results)
         comm.isend(None, dest=status.source)
     
@@ -128,61 +117,14 @@ if rank == 0:
         
     
 else:
-#     for _ in range(rank - 1):
-#         next(local_tiles)
-#     first_tile = next(local_tiles)
     print(rank, "attempting to send blank_results")
-    comm.send({0: (0,)}, dest=0, tag=tag_results)
-    print(rank, "entering while loop")
+    comm.send({0: (0,)}, dest=0)
+    print(rank, "entering the while loop")
     while True:
-        tile = comm.recv(source = 0) # tag=MPI.ANY_TAG
-        #print(rank, "received tile", tile)
+        tile = comm.recv(source = 0)
         if tile:
             results = dummy_job(tile)
         else:
             print(rank, "says goodbye")
             break
         comm.isend(results, dest=0)
-
-
-# sample = np.zeros(5, dtype='float64')
-
-# if rank == 1:
-#     sample = np.ones(5, dtype='float64')
-#     comm.Send([sample, MPI.DOUBLE], dest=0, tag=tag_idle)
-# elif rank == 0:
-#     #for _ in range(1, size):
-#     status = MPI.Status()
-#     comm.Probe(MPI.ANY_SOURCE, MPI.ANY_TAG, status=status)
-#     print(status.tag)
-#     print(status.source)
-#     print(status.Get_elements(MPI.DOUBLE))
-#     comm.Recv(sample, source=status.source)
-
-
-# if rank == 0:
-#     status = MPI.Status()
-#     task_queue = range( (dim0 - 1) // window + 1)
-#     while(task_queue)
-# else:
-#     comm.send(None, dest=0, tag=tag_idle)
-    
-#     request = comm.Irecv(MPI.ANY_SOURCE, 
-#     packet = data[task*window: (task+1)*window]
-#     comm.Send(data, )
-
-
-# if rank != 0:
-#     data = np.empty(data_shape, dtype=dtype)
-
-# if rank == 0:
-#     colsA = [0, 1]
-#     colsB = [2, 3]
-#     column_bunch = data[:, colsA]
-#     comm.Send(column_bunch, dest=1, tag=0)
-#     .
-# elif rank == 1:
-#     column_bunch = np.empty((dim0, 2), dtype=dtype)
-#     comm.Recv(column_bunch, source=0, tag=0)
-
-# print(rank, column_bunch)
